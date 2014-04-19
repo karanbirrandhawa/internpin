1d091804750ce44965165056318dcdf043c45460
Karan <karan@Karans-MacBook-Air.local>
2014-04-18 20:36:16 -0400

    added typehead and bootstrap angular

--- app/js/controller/app.js
+++ app/js/controller/app.js
@@ -7,4 +7,7 @@ angular.module('app').controller('AppCtrl', function($scope, Utils) {
 	    zoom: 12,
 			draggable: true
 	};
+	
+	
+	$scope.options = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 })
